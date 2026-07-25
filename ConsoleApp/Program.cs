d29a6a0c332bf4cd3b1fa7278b9c07bb3566c117
Jonas <jonas@2doo.be>
2026-07-25 23:51:01 +0200

    Flower klasse, Bouquet klasse en Customer klasse volledig afgewerkt naargelang de opgave van de taak

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Domain;
+using System.Drawing;
 
-Console.WriteLine("Hello, World!");
+Flower rose = new Flower("rose", Color.Red, 1.25m);
+Flower tulip = new Flower("tulip", Color.Red, 1.00m);
+Flower daffodil = new Flower("daffodil", Color.Yellow, 1.50m);
+Flower tulip1 = new Flower("tulip", Color.Yellow, 1.00m);
+
+Bouquet valentineBouquet = new Bouquet("Valentine's bouquet");
+valentineBouquet.AddFlower(rose, 10);
+valentineBouquet.AddFlower(rose, 5);
+valentineBouquet.AddFlower(daffodil, 7);
+valentineBouquet.AddFlower(tulip1, 3);
+valentineBouquet.AddFlower(tulip, 5);
+
+Customer alice = new Customer("Alice");
+alice.AddBouquet(valentineBouquet, DateTime.Now);
+Console.WriteLine(alice);
--- Domain/Class1.cs
+++ Domain/Class1.cs
@@ -1,5 +1,95 @@
 ﻿namespace Domain;
+using System.Drawing;
 
-public class Class1
+public class Flower(string name, Color color, decimal price)
 {
+    public string Name { get;} = name;
+    public Color Color { get;} = color;
+    public decimal Price { get;} = price;
+
+    public override string ToString()
+    {
+        return $"{Name} ({Color.Name})";
+    }
+    
 }
+
+public class Bouquet(string name)
+{
+    public string Name { get; } = name;
+    private Dictionary<Flower, int> Flowers { get; } = new Dictionary<Flower, int>();
+    
+    public void AddFlower(Flower flower, int amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount));
+        }
+
+        if (Flowers.ContainsKey(flower))
+        {
+            Flowers[flower] += amount;
+        }
+        else
+        {
+            Flowers.Add(flower, amount);
+        }
+    }
+
+    public decimal ComputePriceBouquet()
+    {
+        decimal amount = 10;
+        foreach (KeyValuePair<Flower, int> flower in Flowers)
+        {
+            amount += flower.Key.Price * flower.Value;
+        }
+
+        return amount;
+    }
+
+    public override string ToString()
+    {
+        string result = $"{Name}\n====\n";
+        IEnumerable<IGrouping<string, KeyValuePair<Flower, int>>> colorGroups = Flowers.GroupBy(f => f.Key.Color.Name);
+        foreach (IGrouping<string, KeyValuePair<Flower, int>> colorGroup in colorGroups)
+        {
+            result += $"{colorGroup.Key}\n";
+            foreach (KeyValuePair<Flower, int> flower in colorGroup)
+            {
+                result += $"  {flower.Value} x {flower.Key}\n";
+            }
+        }
+        result += $"----\nPrice: {ComputePriceBouquet():F2} euro";
+        return result;
+    }
+}
+
+public class Customer(string name)
+{
+    public string Name { get; } = name;
+    private Dictionary<Bouquet, DateTime> _bouquets = new Dictionary<Bouquet, DateTime>();
+
+    public void AddBouquet(Bouquet bouquet, DateTime purchaseDate)
+    {
+        if (_bouquets.ContainsKey(bouquet))
+        {
+            _bouquets[bouquet] = purchaseDate;
+        }
+        else
+        {
+            _bouquets.Add(bouquet, purchaseDate);
+        }
+    }
+
+    public override string ToString()
+    {
+        string result = $"{Name}\n";
+        IEnumerable<KeyValuePair<Bouquet, DateTime>> orderedBouquets = _bouquets.OrderBy(b => b.Value);
+        foreach (KeyValuePair<Bouquet, DateTime> bouquet in orderedBouquets)
+        {
+            result += $"{bouquet.Value:yyyy-MM-dd} {bouquet.Key.Name}\n";
+        }
+        return result.TrimEnd();
+    }
+}
+
